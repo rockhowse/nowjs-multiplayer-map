1efaea5babad055f2a64ff2e992642306a6d6216
Nate Rock <rockhowse@gmail.com>
2012-01-12 23:45:16 -0600

    added in enter key to send message and auto scroll to bottom using jquery =)

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
     now.receiveMessage = function(name, message){
         $("#messages").append("<br>" + name + ": " + message);
+
+        // scroll the div to the bottom
+        $("#messages").scrollTop($("#messages")[0].scrollHeight);
     }
 
+    // add in "enter" key press to trigger "send" click
+    $("#chat-input").keyup(function(event){
+        if(event.keyCode == 13){
+            $("#send-button").click();
+        }
+    });
+
+    // send the message then clear the text input
     $("#send-button").click(function(){
         now.distributeMessage($("#chat-input").val());
         $("#chat-input").val("");
